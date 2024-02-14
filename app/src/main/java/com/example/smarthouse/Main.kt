951758f22cd8577be02a7e2a45508a91a8a94ed1
Alexander Perednya <alexander_perednya@belkatechnologies.com>
2024-02-14 14:29:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/smarthouse/Main.kt
@@ -0,0 +1,30 @@
+package com.example.smarthouse
+
+import java.lang.reflect.Constructor
+
+fun main(){
+
+    val smartTvDevice = SmartDevice("Android TV", "Entertainment")
+
+
+    println("Device name : ${smartTvDevice.name}")
+    smartTvDevice.turnOn()
+    smartTvDevice.turnOff()
+
+}
+class SmartTvDevice(deviceName: String, deviceCategory: String):SmartDevice(name = deviceName, category = deviceCategory){
+
+}
+
+open class SmartDevice(val name: String, val category: String ){
+
+    var deviceStatus = "online"
+
+    fun turnOn(){
+        println("Smart device is turned on.")
+    }
+
+    fun turnOff(){
+        println("Smart device is turned off.")
+    }
+}
