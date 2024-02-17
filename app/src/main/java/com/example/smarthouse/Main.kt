059e4fd21b8422d417939e58e892e07b88ab6bc1
Alexander Perednya <alexander_perednya@belkatechnologies.com>
2024-02-17 20:40:37 +0300

    add

--- app/src/main/java/com/example/smarthouse/Main.kt
+++ app/src/main/java/com/example/smarthouse/Main.kt
@@ -2,30 +2,145 @@ package com.example.smarthouse
 
 import java.lang.reflect.Constructor
 
-fun main(){
+fun main() {
 
-    val smartTvDevice = SmartTvDevice("Android TV", "Entertainment")
+    //val smartTvDevice = SmartTvDevice("Android TV", "Entertainment")
+
+    var smartDevice : SmartDevice = SmartTvDevice("Android TV", "Entertainment")
+    smartDevice.turnOn()
+
+    smartDevice = SmartLightDevice("Google Light", "Utility")
+    smartDevice.turnOn()
 
 
-    println("Device name : ${smartTvDevice.name}")
-    smartTvDevice.turnOn()
-    smartTvDevice.turnOff()
+//    println("Device name : ${smartDevice.name}")
+//    println("Category name : ${smartDevice.category}")
+//    smartDevice.turnOn()
+//    smartDevice.turnOff()
 
 }
-class SmartTvDevice(deviceName: String, deviceCategory: String):SmartDevice(name = deviceName, category = deviceCategory){
 
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    override val deviceType = "Smart Tv"
+    var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    fun increaseSpeakerVolume() {
+        channelNumber++
+        println("Speaker volume increased to $speakerVolume.")
+    }
+
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
+    }
+
+    override fun turnOn(){
+        super.turnOn()
+        println("$name is turned on. Speaker volume is set to $speakerVolume and channel number is \" +\n" +
+                "                \"set to $channelNumber.")
+    }
+
+    override fun turnOff(){
+        super.turnOff()
+        println("$name turned off")
+    }
 }
 
-open class SmartDevice(val name: String, val category: String ){
+class SmartLightDevice(deviceName: String, category: String) :
+    SmartDevice(name = deviceName, category = category) {
+
+    override val deviceType = "Smart Light"
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness increased to $brightnessLevel.")
+    }
+
+    override fun turnOn(){
+        super.turnOn()
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is $brightnessLevel.")
+    }
+
+    override fun turnOff(){
+        super.turnOff()
+        brightnessLevel = 0
+        println("$name turned off. The brightness level is $brightnessLevel.")
+    }
+}
+
+class SmartHome(
+    val smartTvDevice: SmartTvDevice,
+    val smartLightDevice: SmartLightDevice
+) {
+    fun turnOnTv() {
+        smartTvDevice.turnOn()
+    }
+
+    fun turnOffTv() {
+        smartTvDevice.turnOff()
+    }
+
+    fun increaseTvVolume() {
+        smartTvDevice.increaseSpeakerVolume()
+    }
+
+    fun changeTvChannelToNext() {
+        smartTvDevice.nextChannel()
+    }
+
+    fun turnOnLight(){
+        smartLightDevice.turnOn()
+    }
+    fun turnOffLight(){
+        smartLightDevice.turnOff()
+    }
+
+    fun increaseLightBrightness(){
+        smartLightDevice.increaseBrightness()
+    }
+
+    fun turnOffAllDevices(){
+        turnOffTv()
+        turnOffLight()
+    }
+}
+
+open class SmartDevice(val name: String, val category: String) {
+
 
     var deviceStatus = "online"
 
-    fun turnOn(){
-        println("Smart device is turned on.")
+    open val deviceType = "unknown"
+
+    open fun turnOn() {
+        deviceStatus = "On"
+        //println("Smart device is turned on.")
     }
 
-    fun turnOff(){
-        println("Smart device is turned off.")
+    open fun turnOff() {
+        deviceStatus = "Off"
+        //println("Smart device is turned off.")
     }
 }
 
